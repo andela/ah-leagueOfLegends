3658bf83f682af19b7de676cd83d25b8e9a5020a
derkip <derrickkirwa@KE-C02SJ35ZFVH5s-MacBook-Pro.local>
2018-08-28 17:26:14 +0300

    [ch #159965465]  update re_path with path

--- authors/urls.py
+++ authors/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.urls import re_path, path, include
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
