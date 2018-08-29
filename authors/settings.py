cc1c6ad3983c2bdb9a0aef060cf863741f45665f
quantum-35 <quantumcomputing35@icloud.com>
2018-08-29 10:51:46 +0300

    Make test run

--- authors/settings.py
+++ authors/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgres',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'authors_haven',
+        'USER': 'author',
+        'PASSWORD': 'qwertyui123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
