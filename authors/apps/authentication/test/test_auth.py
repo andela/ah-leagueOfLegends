fad51db38c0110315d6361227b12de629a203848
Derrick Korir <derrick.korir@gmail.com>
2018-08-29 19:05:10 +0300

    [chore #159965463]  add login with wrong password test

--- authors/apps/authentication/test/test_auth.py
+++ authors/apps/authentication/test/test_auth.py
@@ -16,19 +16,6 @@ class UserAuthenticationTestCase(BaseTest):
         )
         self.assertEquals(status.HTTP_201_CREATED, response.status_code)
 
-    def test_user_login(self):
-        response = self.client.post(
-               reverse('user_signup'),
-               self.user_cred,
-               format='json')
-        response = self.client.post(
-               reverse('user_login'),
-               self.user_cred,
-               format='json'
-        )
-
-        self.assertEquals(status.HTTP_200_OK, response.status_code)
-
     def test_user_registration_no_email(self):
         response = self.client.post(
                reverse('user_signup'),
@@ -53,3 +40,34 @@ class UserAuthenticationTestCase(BaseTest):
                format='json'
         )
         self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_double_registration(self):
+        response = self.register_user()
+        response = self.register_user()
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_user_login(self):
+        response = self.client.post(
+               reverse('user_signup'),
+               self.user_cred,
+               format='json')
+        response = self.client.post(
+               reverse('user_login'),
+               self.user_cred,
+               format='json'
+        )
+
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+
+    def test_login_wrong_password(self):
+        response = self.client.post(
+               reverse('user_signup'),
+               self.user_cred,
+               format='json')
+        response = self.client.post(
+               reverse('user_signup'),
+               self.user_cred_wrong_pass,
+               format='json')
+
+    
+
