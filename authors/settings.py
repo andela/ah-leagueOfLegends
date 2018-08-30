d803905a51a2891295796a0832dff08464165525
derkip <derrickkirwa@KE-C02SJ35ZFVH5s-MacBook-Pro.local>
2018-08-30 12:35:56 +0300

    ft #159965475] update db settings

--- authors/settings.py
+++ authors/settings.py
@@ -80,12 +80,15 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'authors_haven',
+       'USER': 'postgres',
+       'PASSWORD': 'admin',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
