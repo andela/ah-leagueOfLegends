078577e7b79e8e3f9984efae89438c7397d12acb
rethes <esther.mutua@strathmore.edu>
2018-08-30 11:28:29 +0300

    [ft #159965475] Add JWTAuthentication class

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,10 +1,14 @@
 # import jwt
-#
+
 # from django.conf import settings
-#
-# from rest_framework import authentication, exceptions
-#
+
+from rest_framework import authentication, exceptions
+
 # from .models import User
 
 """Configure JWT Here"""
+
+
+class JWTAuthentication(authentication.TokenAuthentication):
+    pass
 
