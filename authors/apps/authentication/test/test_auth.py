fa14137fc49a0a78c975dc8b704b77ab4251f60d
Derrick Korir <derrick.korir@gmail.com>
2018-09-05 21:23:17 +0300

    [Chore #159965463] Add authentication tests

---
+++ authors/apps/authentication/test/test_auth.py
@@ -0,0 +1,80 @@
+from authors.apps.base_test import BaseTest
+from django.urls import reverse
+from rest_framework.views import status
+from ..models import User
+
+class UserAuthenticationTestCase(BaseTest):
+
+    def test_user_can_register(self):
+        """Test whether user gets a valid token on registration"""
+        response = self.client.post(
+               reverse('authentication:user_signup'),
+               self.user_cred,
+               format='json'
+        )
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+    def test_user_receives_token_on_registration(self):
+        pass
+
+    def test_verification_email_sent_on_registration(self):
+        pass
+
+
+    def test_user_registration_no_email(self):
+        response = self.client.post(
+               reverse('authentication:user_signup'),
+               self.user_cred_no_email,
+               format='json'
+        )
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_user_registration_no_username(self):
+        response = self.client.post(
+               reverse('authentication:user_signup'),
+               self.user_cred_no_username,
+               format='json'
+        )
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+
+    def test_user_registration_no_details(self):
+        response = self.client.post(
+               reverse('authentication:user_signup'),
+               self.user_cred_no_details,
+               format='json'
+        )
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_double_registration(self):
+        response = self.register_user()
+        response = self.register_user()
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+
+    def test_user_login(self):
+        response = self.client.post(
+               reverse('authentication:user_signup'),
+               self.user_cred,
+               format='json')
+        response = self.client.post(
+               reverse('authentication:user_login'),
+               self.user_cred,
+               format='json'
+        )
+
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+
+    def test_login_wrong_password(self):
+        response = self.client.post(
+               self.SIGN_UP_URL,
+               self.user_cred,
+               format='json')
+        response = self.client.post(
+               reverse('authentication:user_login'),
+               self.user_cred_wrong_pass,
+               format='json')
+
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    
