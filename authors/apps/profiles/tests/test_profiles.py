d668213abe796d8cd5350940a7340e4690e77213
Derrick Korir <derrick.korir@gmail.com>
2018-09-05 21:24:34 +0300

    [Chore #159965463] Add skeleton tests for profile

---
+++ authors/apps/profiles/tests/test_profiles.py
@@ -0,0 +1,18 @@
+from authors.apps.base_test import BaseTest
+from django.urls import reverse
+from rest_framework.views import status
+
+class ProfileTestCase(BaseTest):
+
+    def test_user_can_create_profile(self):
+        pass
+
+    def test_user_can_view_profile(self):
+        pass
+
+    def test_user_can_edit_profile(self):
+        pass
+
+    def test_users_can_edit_only_their_profie(self):
+        pass
+
