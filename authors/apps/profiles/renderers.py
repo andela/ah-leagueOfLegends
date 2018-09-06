58d80041d97449c1f60e66023a1691b64e69f2ec
derkip <dkip64@gmail.com>
2018-09-06 17:27:44 +0300

    [Feature #159965480] Update object name in renderer

--- authors/apps/profiles/renderers.py
+++ authors/apps/profiles/renderers.py
@@ -5,11 +5,10 @@ from rest_framework.renderers import JSONRenderer
 
 class ProfileJSONRenderer(JSONRenderer):
     charset = 'utf-8'
-    object_label = 'object'
+    object_label = 'profile'
 
     def render(self, data, media_type=None, renderer_context=None):
         errors = data.get('errors', None)
-
         if errors is not None:
             return super(ProfileJSONRenderer, self).render(data)
 
