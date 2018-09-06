4f1c8b8d5f4b1239a3283be18792c0546129468c
quantum-35 <quantumcomputing35@icloud.com>
2018-09-06 21:13:56 +0300

    [Feature #159965477] Update travis file

--- authors/test_setting.py
+++ authors/test_setting.py
@@ -1,5 +1,5 @@
 from authors.settings import *
 
-CELERY_TASK_ALWAYS_EAGER = True
-CELERY_TASK_EAGER_PROPOGATES = True 
-Debug = True
+CELERY_TASK_ALWAYS_EAGER=True
+CELERY_TASK_EAGER_PROPOGATES=True 
+Debug=True
