5ee6997d085568dbfcedd56b2212770d83954d1d
derkip <dkip64@gmail.com>
2018-09-06 09:58:53 +0300

    [Feature #159965480] Create Profile model

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """ This class represents the user Profile model """
+    # There is one-to-one relationship between the user an profile model
+    # A user will only have one related Profile model
+    user = models.OneToOneField(
+        'authentication.User', on_delete=models.CASCADE
+    )
+
+    bio = models.TextField(blank=True)
+    image = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
