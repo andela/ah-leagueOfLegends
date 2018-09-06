829b21219c246aceaf8795cc0cdfaf9b8d00fc7f
quantum-35 <quantumcomputing35@icloud.com>
2018-09-06 12:54:31 +0300

    [Feature #159965477] Add test for password reset and add token generator file

---
+++ authors/apps/authentication/test/test_auth.py
@@ -0,0 +1,48 @@
+from django.urls import reverse
+from rest_framework.views import status
+from django.core import mail
+from rest_framework.authtoken.models import Token
+
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from ..models import User
+from django.core import mail
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from rest_framework.test import force_authenticate
+from rest_framework.test import APIRequestFactory
+
+from ..models import User
+from ..views import Verify
+from authors.apps.base_test import BaseTest
+from ..chk_token import authcheck_token
+
+
+class EmailAuthenticationTestCase(BaseTest):
+
+    def test_email_is_sent(self):
+        pass
+
+    def test_user_can_reset_password(self):
+        '''Test registered users can reset their password by sending token
+        reset token to their emails.
+        '''
+        response = self.register_user()
+        user = User.objects.get()
+        token = authcheck_token.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode("utf-8")
+        request = APIRequestFactory().get(
+            reverse("authentication:verify", args=[uid, token]))
+        verify_account = Verify.as_view()
+        response = verify_account(request, uidb64=uid, token=token)
+        self.assertTrue(response.status_code, 200)
+        user = User.objects.get()
+        self.assertTrue(user.confirmed_user)
+
+
+    def test_user_acccount_verified(self):
+        pass
+
+    def test_invalid_verification_link(self):
+        pass
