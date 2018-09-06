acc812355bf811203b1eef733899e05b9e932cbd
derkip <dkip64@gmail.com>
2018-09-06 17:28:50 +0300

    [Feature #159965480] Update generate_avatar method

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -6,21 +6,25 @@ import random
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
+    is_active = serializers.CharField(source='user.is_active')
+    date_joined = serializers.CharField(source='user.created_at')
     bio = serializers.CharField(allow_blank=True, required=False)
     image = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image',)
+        fields = ('username', 'bio', 'image','is_active','date_joined')
         read_only_fields = ('username',)
-
-    def get_image(self, obj):
-        if obj.image:
-            return obj.image
-        return generate_avatar()
-
+    
     def generate_avatar(self):
         random_number = random.randint(0, 99999999)
         avatar_url = 'https://api.adorable.io/avatars/100/'
         return avatar_url + str(random_number)
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image
+        return self.generate_avatar()
+
+    
         
