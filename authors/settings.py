b34d9cd5447affcef15c64dbf1eb3536b12f94b1
Mutuba <daniel.mutuba@andela.com>
2018-09-07 13:25:05 +0300

    [Feature #159965481] Fixes merge conflicts

--- authors/settings.py
+++ authors/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = dict(default=dj_database_url.config())
 
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
