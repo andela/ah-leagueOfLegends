f32aec917c8e3b5fb7b1aa7ddd4c4f14e4eaef4a
quantum-35 <quantumcomputing35@icloud.com>
2018-09-07 09:35:06 +0300

    [Feature #159965477] Add test_settings to manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     settings = 'authors.test_settings' if 'test' in sys.argv else 'authors.settings'
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
