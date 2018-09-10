49cbd21ae2a861c44998065841b7bbdc386900ab
Teddykavooh <teddykavooh@gmail.com>
2018-09-10 17:23:14 +0300

    [Feature #159965479] Add social login tests

--- authors/apps/authentication/test/test_social_login.py
+++ authors/apps/authentication/test/test_social_login.py
@@ -1,5 +1,51 @@
-from authors.apps.base_test import BaseTest
 from django.urls import reverse
-from rest_framework.views import status
-from ..models import User
+from rest_framework.test import APITestCase, APIClient
 
+
+class SocialAuthenticationTests(APITestCase):
+    def setUp(self):
+        """
+        Setup for tests
+        """
+        # Set up the social_auth url.
+        self.auth_url = reverse('authentication:social_auth')
+        self.client = APIClient()
+
+
+def test_missing_access_token(self):
+    """
+    Test request without passing the access token
+    """
+    data = {"provider": "github"}
+    response = self.client.post(self.auth_url, data=data)
+    self.assertEqual(response.status_code, 400)
+
+
+def test_missing_provider(self):
+    """
+    Test request without passing the provider
+    """
+    access_token = "7e19b118feb4357fe0057a2e8a6c9f9cea668552"
+    data = {"access_token": access_token}
+    response = self.client.post(self.auth_url, data=data)
+    self.assertEqual(response.status_code, 400)
+
+
+def test_invalid_provider(self):
+    """
+    Test giving a non-existent provider
+    """
+    access_token = "authorshaven"
+    data = {"access_token": access_token, "provider": "facebook-oauth23"}
+    response = self.client.post(self.auth_url, data=data)
+    self.assertEqual(response.status_code, 400)
+
+
+def test_invalid_token(self):
+    """
+    Test an invalid access token
+    """
+    access_token = "authorshaven"
+    data = {"access_token": access_token, "provider": "facebook"}
+    response = self.client.post(self.auth_url, data=data)
+    self.assertEqual(response.status_code, 400)
