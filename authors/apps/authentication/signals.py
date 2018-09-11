8fe889cc69292c49903070ae32e0f444025337d0
derkip <dkip64@gmail.com>
2018-09-11 13:38:09 +0300

    [Feature #159965480] Update signals to save generated avatar upon User instatiatioin

--- authors/apps/authentication/signals.py
+++ authors/apps/authentication/signals.py
@@ -1,3 +1,4 @@
+import random
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,4 +10,12 @@ from .models import User
 def save_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile(user=instance)
+        profile.image = generate_avatar()
         profile.save()
+        
+def generate_avatar():
+    """Genarates an avatar and saves to the user model upon instantiation"""
+    random_number = random.randint(0, 99999999)
+    avatar_url = 'https://api.adorable.io/avatars/500/'
+    image = avatar_url + str(random_number)
+    return image 
