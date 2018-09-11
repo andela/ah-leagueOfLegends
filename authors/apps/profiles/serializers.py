308f5f2a34da7786f272af4f8d49d9e885517e81
derkip <dkip64@gmail.com>
2018-09-11 13:34:50 +0300

    [Feature #159965480] Update serializers to return relevant user detail

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -2,29 +2,18 @@ from rest_framework import serializers
 
 from .models import Profile
 
-import random
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
-    is_active = serializers.CharField(source='user.is_active')
     date_joined = serializers.CharField(source='user.created_at')
     bio = serializers.CharField(allow_blank=True, required=False)
-    image = serializers.SerializerMethodField()
+    image = serializers.URLField()
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image','is_active','date_joined')
+        fields = ('username', 'bio', 'image','date_joined')
         read_only_fields = ('username',)
     
-    def generate_avatar(self):
-        random_number = random.randint(0, 99999999)
-        avatar_url = 'https://api.adorable.io/avatars/100/'
-        return avatar_url + str(random_number)
-
-    def get_image(self, obj):
-        if obj.image:
-            return obj.image
-        return self.generate_avatar()
 
     
         
