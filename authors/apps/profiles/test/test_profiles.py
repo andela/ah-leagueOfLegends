338ff28b8d1121dfe18bfb6a1761b6c9581eca31
derkip <dkip64@gmail.com>
2018-09-18 11:33:48 +0300

    [Feature #159965490] Add tests for retrieving  all user profiles list

--- authors/apps/profiles/test/test_profiles.py
+++ authors/apps/profiles/test/test_profiles.py
@@ -13,7 +13,7 @@ class ProfileTestCase(BaseTest):
                 "bio":"Through the lense",
             }
             }
-        
+    # helper methods  
     def update_user_bio(self, token,bio):
         """Helper method to update an user bio"""
         return self.client.put(
@@ -30,6 +30,14 @@ class ProfileTestCase(BaseTest):
             HTTP_AUTHORIZATION='Bearer ' + token,
             format='json'
         )
+    def retrieve_profiles(self, token):
+        """Helper method to update an user bio"""
+        return self.client.get(
+            '/api/profiles',
+            HTTP_AUTHORIZATION='Bearer ' + token,
+        )
+
+    # unit tests
 
     def test_user_can_create_profile(self):
         """ Test if user profile is created """ 
@@ -48,22 +56,51 @@ class ProfileTestCase(BaseTest):
 
     
     def test_update_bio(self):
-            """ 
-            Tests whether a user can update his/her profile bio
-            """
-            self.register_user()
-            response = self.login_user()
-            token = response.data['token']
-            response = self.update_user_bio(token,self.bio)
-            self.assertEquals(status.HTTP_200_OK, response.status_code)
+        """ 
+        Tests whether a user can update his/her profile bio
+        """
+        self.register_user()
+        response = self.login_user()
+        token = response.data['token']
+        response = self.update_user_bio(token,self.bio)
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
 
     def test_update_image(self):
-            """ 
-            Tests whether a user can update his/her profile bio
-            """
-            self.register_user()
-            response = self.login_user()
-            token = response.data['token']
-            response = self.update_user_image(token,self.image)
-            self.assertEquals(status.HTTP_200_OK, response.status_code)
+        """ 
+        Tests whether a user can update his/her profile bio
+        """
+        self.register_user()
+        response = self.login_user()
+        token = response.data['token']
+        response = self.update_user_image(token,self.image)
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+    
+    def test_list_profiles(self):
+        """ 
+        Tests whether a user profile can be listed after authentication.
+        """
+        self.register_user()
+        response = self.login_user()
+        token = response.data['token']
+        response = self.retrieve_profiles(token)
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+
+    def test_list_profiles_without_authentication(self):
+        """ 
+        Tests whether a user profile can be listed without authentication.
+        """
+        self.register_user()
+        token = ''
+        response = self.retrieve_profiles(token)
+        self.assertEquals(status.HTTP_403_FORBIDDEN, response.status_code)
+
+    def test_paginated_profile_list(self):
+        """
+        Test user can view paginated data
+        """
+        self.register_user()
+        response = self.login_user()
+        token = response.data['token']
+        response = self.retrieve_profiles(token)
+        self.assertEquals(response.data['count'], 1)
     
