fe9a6d17fbb6a5be83480226eee3e8718a4962b1
derkip <dkip64@gmail.com>
2018-09-19 23:04:11 +0300

    [Feature #159965486] Update Urls

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,9 +1,12 @@
-from django.urls import include, re_path,path
+from django.urls import include, path
 
-from .views import ProfileRetrieveAPIView, ProfileList
+from .views import ProfileRetrieveAPIView, ProfileFollowAPIView, \
+                  FollowersAPIView, FollowingAPIView
 
 app_name = 'profiles'
 urlpatterns = [
-  path('', ProfileList.as_view()),
-  re_path(r'(?P<username>\w+)?$', ProfileRetrieveAPIView.as_view()),
+  path('<str:username>', ProfileRetrieveAPIView.as_view()),
+  path('<str:username>/follow', ProfileFollowAPIView.as_view()),
+  path('<str:username>/following', FollowingAPIView.as_view()),
+  path('<str:username>/followers', FollowersAPIView.as_view()),
 ] 
