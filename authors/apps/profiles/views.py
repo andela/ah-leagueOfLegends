645eeeebcddb03d01faf65b1e1fc8b3259baa405
derkip <dkip64@gmail.com>
2018-09-19 23:03:29 +0300

    [Feature #159965486] Create views for following/unfollowing and listing followers

--- authors/apps/profiles/views.py
+++ authors/apps/profiles/views.py
@@ -1,6 +1,7 @@
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 
@@ -24,7 +25,9 @@ class ProfileRetrieveAPIView(RetrieveAPIView):
         except Profile.DoesNotExist:
             raise ProfileDoesNotExist
 
-        serializer = self.serializer_class(profile)
+        serializer = self.serializer_class(profile, 
+                           context={'request': request} )
+
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -44,3 +47,66 @@ class ProfileList(ListAPIView):
            many=True
         )
         return self.get_paginated_response(serializer.data)
+
+class ProfileFollowAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def put(self, request, username=None):
+        follower = self.request.user.profile
+        try:
+            followee = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise ProfileDoesNotExist
+
+        if follower.pk is followee.pk:
+            raise serializers.ValidationError('You can not follow yourself.')
+
+        if follower.is_following(followee) is False:
+            follower.follow(followee)
+        else:
+            follower.unfollow(followee)
+
+        serializer = self.serializer_class(followee, context={
+            'request': request
+        })
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class FollowersAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def get(self, request, username):
+        user = self.request.user.profile
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise ProfileDoesNotExist
+    
+        followers = user.get_followers(profile)
+        serializer = self.serializer_class(followers,
+                                    many=True,context={'request': request})
+        return Response({"followers":serializer.data},
+                                    status=status.HTTP_200_OK, )
+     
+
+
+class FollowingAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def get(self, request, username):
+        user = self.request.user.profile
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise ProfileDoesNotExist
+    
+        following = user.get_following(profile)
+        serializer = self.serializer_class(following,many=True,
+                                        context={'request': request})
+        return Response({"followers":serializer.data},
+                                    status=status.HTTP_200_OK, )
