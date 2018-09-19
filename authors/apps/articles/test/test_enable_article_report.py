19efc9f91d2c22eb6dd417a04188c9504545c989
Teddykavooh <teddykavooh@gmail.com>
2018-09-19 12:20:27 +0300

    [Feature #159965499] Add more tests

--- authors/apps/articles/test/test_enable_article_report.py
+++ authors/apps/articles/test/test_enable_article_report.py
@@ -29,3 +29,73 @@ class ReportArticleTestCase(ArticleTestCase):
         response = self.client.put(path='/api/articles/how-to-train-your-dragon/report')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.data['detail'], "Authentication credentials were not provided.")
+
+    def test_if_none_admin_user_can_get_reports(self):
+        self.register_user()
+        res = self.client.post(
+            reverse('authentication:user_login'),
+            self.user_cred,
+            format='json')
+        token = res.data['token']
+        response = self.client.get(path='/api/articles/how-to-your-dragon/reports',
+                                   HTTP_AUTHORIZATION='Bearer ' + token)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_if_user_can_report_articles(self):
+        self.register_user()
+        res = self.client.post(
+            reverse('authentication:user_login'),
+            self.user_cred,
+            format='json')
+        token = res.data['token']
+        article = {
+            "article":
+                {
+                    "title": "How to feed your dragon",
+                    "description": "Wanna know how?",
+                    "body": "You don't believe?",
+                }
+        }
+        response = self.create_article(token, article)
+
+        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
+        data = {"report": {"body": "A lot of plagiarism"}}
+        response = self.client.post(path='/api/articles/how-to-feed-your-dragon/report', data=data,
+                                    HTTP_AUTHORIZATION='Bearer ' + token, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_if_user_can_report_articles_without_body(self):
+        self.register_user()
+        res = self.client.post(
+            reverse('authentication:user_login'),
+            self.user_cred,
+            format='json')
+        token = res.data['token']
+        article = {
+            "article":
+                {
+                    "title": "How to feed your dragon",
+                    "description": "Wanna know how?",
+                    "body": "You don't believe?",
+                }
+        }
+        response = self.create_article(token, article)
+
+        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
+        response = self.client.post(path='/api/articles/how-to-feed-your-dragon/report',
+                                    HTTP_AUTHORIZATION='Bearer ' + token, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_if_user_can_report_missing_article(self):
+        self.register_user()
+        res = self.client.post(
+            reverse('authentication:user_login'),
+            self.user_cred,
+            format='json')
+        token = res.data['token']
+        response = self.client.post(path='/api/articles/how-to-your-dragon/report',
+                                    HTTP_AUTHORIZATION='Bearer ' + token)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEquals(response.data['detail'], 'An article with that slug does not exist')
+
+
