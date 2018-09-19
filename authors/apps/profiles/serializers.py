772243449205c6be5da4c48e891ade6f6a4cb5c8
rethes <esther.mutua@strathmore.edu>
2018-09-19 19:12:08 +0300

    [Feature #159965498] Add bookmarks field to the ProfileSerializers

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -8,12 +8,17 @@ class ProfileSerializer(serializers.ModelSerializer):
     date_joined = serializers.CharField(source='user.created_at')
     bio = serializers.CharField(allow_blank=True, required=False)
     image = serializers.URLField()
+    bookmarks = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='title'
+    )
     following = serializers.SerializerMethodField()
     following = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ('username', 'bio', 'image','date_joined','following',)
+        fields = ('username', 'bio', 'image','date_joined','following','bookmarks',)
         read_only_fields = ('username',)
         
     def get_following(self, instance):
@@ -25,7 +30,3 @@ class ProfileSerializer(serializers.ModelSerializer):
         follower = request.user.profile
         followee = instance
         return follower.is_following(followee)
-    
-
-    
-        
