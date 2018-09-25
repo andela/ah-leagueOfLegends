dc10b77e328faeb9d0386fe15d05a085344289ed
Derrick Korir <derrick.korir@gmail.com>
2018-09-25 11:41:54 +0300

    [Chore #160747962] Add single notification view

--- authors/apps/ah_notifications/views.py
+++ authors/apps/ah_notifications/views.py
@@ -2,7 +2,7 @@
 Notifications view
 '''
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.views import APIView	
 from rest_framework.permissions import IsAuthenticated
 from .serializers import NotificationSerializer
@@ -20,9 +20,7 @@ class NotificationViewList(APIView):
     
 
     def get(self, request):
-        queryset = self.request.user.notifications.unread()
-        for notification in queryset:
-            notification.mark_as_read()
+        queryset = self.request.user.notifications.all()
 
         serializer = self.serializer_class(
             queryset,
@@ -30,3 +28,21 @@ class NotificationViewList(APIView):
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class NotificationRetrieveView(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    Retrieve a single notification marking it as read
+    '''
+    lookup_url_kwarg = 'pk'
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (NotificationJSONRenderer,)
+    serializer_class = NotificationSerializer
+
+    def retrieve(self, request, **kwargs):
+        pk = self.kwargs['pk']
+        notification = self.request.user.notifications.get(pk=pk)
+        notification.mark_as_read()
+        serializer = self.serializer_class(
+            notification
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
