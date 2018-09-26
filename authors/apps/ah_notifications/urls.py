e0c551e30d7646c4339b98b995dcb0a6d3164929
Derrick Korir <derrick.korir@gmail.com>
2018-09-26 09:46:00 +0300

    [Feature #160747962] Add urls for toggling notifications

--- authors/apps/ah_notifications/urls.py
+++ authors/apps/ah_notifications/urls.py
@@ -5,9 +5,12 @@ from django.conf.urls import include, url
 from django.urls import path
 
 from .views import NotificationViewList, NotificationRetrieveView
+from authors.apps.profiles.views import SubscribeNotificationAPIView, UnsubscribeNotificationAPIView
 
 urlpatterns = [
     path('notifications', NotificationViewList.as_view()),
+    path('notifications/subscribe', SubscribeNotificationAPIView.as_view()),
+    path('notifications/unsubscribe', UnsubscribeNotificationAPIView.as_view()),
     path('notifications/<pk>', NotificationRetrieveView.as_view()),
 
 ]
