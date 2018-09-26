916a3952e664346619f85f7fb69245e1691c373e
Derrick Korir <derrick.korir@gmail.com>
2018-09-26 09:43:52 +0300

    [Chore #160747962] Add logic to atch non existent notification

--- authors/apps/ah_notifications/views.py
+++ authors/apps/ah_notifications/views.py
@@ -34,15 +34,22 @@ class NotificationRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     Retrieve a single notification marking it as read
     '''
     lookup_url_kwarg = 'pk'
+    queryset = Notification.objects.all()
     permission_classes = (IsAuthenticated,)
     renderer_classes = (NotificationJSONRenderer,)
     serializer_class = NotificationSerializer
 
     def retrieve(self, request, **kwargs):
         pk = self.kwargs['pk']
-        notification = self.request.user.notifications.get(pk=pk)
+        try:
+            notification = self.request.user.notifications.get(pk=pk)
+        except Exception:
+            return Response({"message": "The notification does not exist."},
+                             status=status.HTTP_404_NOT_FOUND)
+        
         notification.mark_as_read()
         serializer = self.serializer_class(
             notification
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
+
